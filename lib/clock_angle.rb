6b9b461ec390ce68558a9a31b196fbd05256bd11
Kyle Boe & Michael Derfler <student@epicodus.com>
2014-07-22 12:47:21 -0700

    refactor again

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,8 +1,5 @@
 
 
 def clock_angle(hour, min)
-  x = (hour * 30) + (min * 0.5)
-  hour_deg = x%360
-  min_deg = min * 6
-  (min_deg - hour_deg).abs
+  ((min * 6) - (((hour * 30) + (min * 0.5))%360)).abs
 end
